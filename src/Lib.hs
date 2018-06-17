38fc7cbcf0eb48cb10c2f369ef05441452512028
noavarice <noavarice@gmail.com>
2018-06-18 02:35:28 +0300

    working 'skips' example

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,21 @@
 module Lib
-    ( someFunc
+    ( skips
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+nthHelper :: [a] -> Int -> Int -> [a]
+nthHelper [] _ _ = []
+nthHelper list n cur =
+  if mod cur n == 0
+    then (head list) : next
+    else next
+  where
+    next = nthHelper (tail list) n (cur + 1)
+
+nth :: [a] -> Int -> [a]
+nth [] _ = []
+nth list n = nthHelper list n 1
+
+skips :: [a] -> [[a]]
+skips [] = []
+skips [x] = [[x]]
+skips list = list : map (nth list) [2..length list]
