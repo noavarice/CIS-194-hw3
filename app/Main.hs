51989efc897a78e092018f276d59c71df9afd15e
noavarice <noavarice@gmail.com>
2018-06-23 14:28:24 +0300

    making histograms

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,6 @@ module Main where
 import Lib
 
 main :: IO ()
-main = print $ localMaxima [1, 4, 3]
+main = putStrLn $ histogram list
+  where
+    list = [0, 2, 2, 1, 2, 1, 1, 2, 3, 5, 9, 8, 8, 8, 0]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,10 @@
 module Lib
     ( skips
     , localMaxima
+    , histogram
     ) where
+
+import Data.List
 
 -- 1. Hopscotch
 
@@ -41,3 +44,19 @@ localMaxima list@(prev:curr:xs) =
   if length list < 3
     then []
     else localMaximaHelper prev curr xs
+
+-- 3. Histogram
+getSymbol :: Integer -> Integer -> Char
+getSymbol limit number = if number >= limit then '*' else ' '
+
+makeLine :: [Integer] -> Integer -> String
+makeLine list limit = map (getSymbol limit) list
+
+makeHistogram :: [Integer] -> String
+makeHistogram list = intercalate "\n" $ (map (makeLine list) $ reverse [1..maximum list]) ++ ["==========\n0123456789\n"]
+
+countEntries :: [Integer] -> Integer -> Integer
+countEntries list number = toInteger $ length $ filter (==number) list
+
+histogram :: [Integer] -> String
+histogram list = makeHistogram $ map (countEntries list) [0..9]
